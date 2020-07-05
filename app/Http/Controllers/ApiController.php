ff0b800a3e4e6e55393b94deb5b954b91e87e62d
yadeepak <deepaky19196@gmail.com>
2020-07-05 16:20:38 +0530

    initial login n register

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+use App\Http\Resources\UserResource;
+
+class ApiController extends Controller
+{
+    //
+    public function login(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        if (!empty($user)) {
+            if (!Hash::check($request->password, $user->password)) {
+                return response()->json(['success'=>false, 'message' => 'Login Fail, pls check password']);
+             }
+                return new UserResource($user);
+        }else{
+            return json_encode(['error' => 'Login Fail, pls check  email']);
+        }
+    }
+    public function register(Request $request)
+    {
+        $user = new User;
+        $user->email = $request->email;
+        $user->name = $request->name;
+        $user->password = Hash::make($request->password);
+        $user->save();
+        return new UserResource($user);
+    }
+}
